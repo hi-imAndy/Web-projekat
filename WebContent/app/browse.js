c9547395de3e93b8a2c2d9d4b287723d5349c988
Repovic_A <aleksarep@uns.ac.rs>
2020-09-03 18:18:28 +0200

    tabela apartmana

--- WebContent/app/browse.js
+++ WebContent/app/browse.js
@@ -1,3 +1,4 @@
+
 Vue.component("browse", {
 	data: function(){
 		return{
@@ -45,13 +46,15 @@ Vue.component("browse", {
 						    </tr>
 						  </thead>
 						  <tbody>
-									<tr v-for="u in apartments" v-on:click="selectApartment(u)" v-bind:class = "{selected : selected.id === u.id}">
-									<img v-bind:src="u.pictures[1]" class="img-fluid img-thumbnail " width="250" height="100">
-									<td>{{u.id }}</td>
-									<td>{{u.id }}</td>
-									<td>{{u.id}}</td>
-									<td>{{u.id }}</td>
+									<tr v-for="u in apartments" v-on:click="selectApartment(u)" v-bind:class = "{selected : selected.id === u.id}" data-toggle="modal" data-target="#apartmentInfo">
+										<td><img v-bind:src="u.pictures[0]" class="img-fluid img-thumbnail " width="250" height="100"></td>
+										<td>{{u.id }}</td>
+										<td>{{u.user.username }}</td>
+										<td>{{u.location.address.city.name}}</td>
+										<td>{{u.location.address.street}} {{u.location.address.number}}</td>
+										
 							    </tr>
+			
 						  </tbody>
 						</table>   
 				    </div>
@@ -60,6 +63,103 @@ Vue.component("browse", {
 			</div>
 			
 			
+			
+			
+			
+				<div id="apartmentInfo"  class = "modal modal-xl  fade">
+				  <div class="modal-dialog modal-xl" role="document" >
+					    <div class="modal-content">
+						      <div class="modal-header">
+								        <h1 class="modal-title">{{selected.id}} informations</h1>
+								        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+								         	 <span aria-hidden="true">&times;</span>
+								        </button>
+							      </div>
+							      <div class="modal-body">
+							        	<table class="table table-image" >
+											<div v-for="(p, index) in selected.pictures">
+							    			<tr v-if="index % 3 == 0 ">
+									        	<td>
+									            	 <img class="d-block w-100" v-bind:src="selected.pictures[index]"  width="560" height="315" style = "margin-left : 15px" >
+									       		 </td>
+									        	<td>
+									            	 <img class="d-block w-100" v-bind:src="selected.pictures[index+1]" v-if="selected.pictures[index+1] != null" width="560" height="315" style = "margin-left : 15px" >
+									        	</td>
+									       		 <td>
+									            	 <img class="d-block w-100" v-bind:src="selected.pictures[index+2]" v-if="selected.pictures[index+2] != null" width="560" height="315" style = "margin-left : 15px " >
+									       		 </td>
+							    			</tr>        
+							 </div>
+				 			 </table>
+
+							</br></br>
+							<div class="row " style="margin-left :40px"  >
+						  		<div class="col-sm" style="margin-left: 10px">
+				             		  <h1><span class="badge badge-primary" style="margin-left: 10px" v-if = "selected.apartmentType === 'ENTIRE_APARTMENT'">Entire apartment</span></h1> 
+							  		  <h1><span class="badge badge-primary" style="margin-left: 10px" v-if = "selected.apartmentType === 'ROOM'" >Room	</span></h1>      
+						    	</div>
+							    <div class="col-sm">
+									<h1><span class="badge badge-success" style="margin-left: 10px">{{selected.numberOfRooms}} rooms</span></h1> 
+							    </div>
+							    <div class="col-sm">
+									<h1><span class="badge badge-danger" style="margin-left: 10px">{{selected.numberOfGuests}} guests</span></h1> 
+							    </div>
+							    <div class="col-sm">
+							       <h1><span class="badge badge-warning" style="margin-left: 10px">{{selected.pricePerNight}} RSD per night</span></h1> 
+							    </div>
+							    <div class="col-sm">
+							    </div>
+							    <div class="col-sm">
+							    </div>
+							    <div class="col-sm">
+							    </div>					
+							</div> 
+					
+
+							<ul>
+								</br></br>
+								<li v-for="am in selected.amenities">
+									<input type="checkbox" id="am.id" value="am"  disabled="disabled" checked="checked">
+									<label for="am.id"> {{am.name}}</label>
+								</li>
+							</ul>
+
+
+
+
+							      </div>
+							      <div class="modal-footer">
+								        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+						     	 </div>
+					    </div>
+				  </div>
+			</div>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+
+				
+				
+
+				
+
+
+				
+				
+			</div>
+		</div>
+		</div>
+		</div>
+	</div>
+
+			
+
 			
 			
 	</div>
@@ -80,3 +180,6 @@ Vue.component("browse", {
     	}
 	},
 });
+
+
+
